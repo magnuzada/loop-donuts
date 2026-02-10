4a8c5f6b37502c848f76897aa185086f69a57759
Magnum Werneck Louzada <cubanojf@gmail.com>
2026-02-09 23:33:01 -0300

    feat: Adicionando Webhook do Mercado Pago

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -3,16 +3,19 @@ import mongoose, { Schema, model, models } from "mongoose";
 const ProductSchema = new Schema(
   {
     name: { type: String, required: true },
-    // 👇 MUDANÇA AQUI: required: false (Opcional)
-    description: { type: String, required: false }, 
+    description: { type: String },
     price: { type: Number, required: true },
     category: { type: String, default: "Tradicional" },
     image: { type: String },
     stock: { type: Number, default: 0 },
+    
+    // 👇 AQUI ESTÁ A CURA! Adicione esta linha:
+    status: { type: String, default: "active" }, 
   },
   { timestamps: true }
 );
 
+// Se o modelo já existe, usa ele. Se não, cria um novo.
 const Product = models.Product || model("Product", ProductSchema);
 
 export default Product;
